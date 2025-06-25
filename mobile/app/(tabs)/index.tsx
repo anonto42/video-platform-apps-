c31afd2bffaf1d8a3f4ef46e4e0f48df9c79d593
MD Sohidul Islam Ananto <anontom90@gmail.com>
2025-06-25 14:58:44 +0600

    icon pack

--- mobile/app/(tabs)/index.tsx
+++ mobile/app/(tabs)/index.tsx
@@ -6,6 +6,8 @@ import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
 
+import DotBar from '@expo/vector-icons/MaterialIcons';
+
 export default function HomeScreen() {
   return (
     <ParallaxScrollView
